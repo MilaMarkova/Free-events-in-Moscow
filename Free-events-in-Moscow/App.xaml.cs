37046dd30ccc66fdd7481f3a7251076f2a30987c
Lyudmila Markova <milamarkova1998@gmail.com>
2017-02-19 13:53:29 +0300

    Добавьте файлы проекта.

---
+++ Free-events-in-Moscow/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Free_events_in_Moscow
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
